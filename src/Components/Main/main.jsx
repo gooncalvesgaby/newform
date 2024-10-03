0f275ef89231f150897081c929d9f775ae3b1a8a
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-10-03 12:16:17 -0300

    inserindo inputs na main

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -3,7 +3,11 @@ import * as S from "./mainStyle.jsx"
 function Main() {
     return(
         <S.Form>
-
+            <S.Section>
+                <S.Input type="text" placeholder="Digite seu nome" />
+                <S.Input type="text" placeholder="Digite seu CPF" />
+                <S.Input type="email" placeholder="Informe seu email" />
+            </S.Section>
         </S.Form>
     )
 }
--- src/Components/Main/mainStyle.jsx
+++ src/Components/Main/mainStyle.jsx
@@ -1,5 +1,15 @@
 import styled from "styled-components"
 
-export const Form = styled.div`
+export const Form = styled.form`
+height: 70vh;
+min-width: 100vw;
+background-color: #edc8c824;
+`
+
+export const Section = styled.div`
+
+`
+
+export const Input = styled.input`
 
 `
