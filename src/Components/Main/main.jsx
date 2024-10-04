ca77f74bbdda7dd71b2affc9a77e818a60661c03
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-10-04 13:27:51 -0300

    inserindo input

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -7,6 +7,14 @@ function Main() {
                 <S.Input type="text" placeholder="Digite seu nome" />
                 <S.Input type="text" placeholder="Digite seu CPF" />
                 <S.Input type="email" placeholder="Informe seu email" />
+                <label>Selecione seu Estado</label>
+                <select>
+                    <option>SP</option>
+                    <option>RN</option>
+                    <option>RJ</option>
+                    <option>BA</option>
+                    <option>CE</option>
+                </select>
             </S.Section>
         </S.Form>
     )
--- src/Components/Main/mainStyle.jsx
+++ src/Components/Main/mainStyle.jsx
@@ -4,6 +4,8 @@ export const Form = styled.form`
 height: 70vh;
 min-width: 100vw;
 background-color: #edc8c824;
+display: flex;
+justify-content: center;
 `
 
 export const Section = styled.div`
