d05e35e36e3feab9811694d6843e0571c12b325d
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-10-08 14:10:34 -0300

    aplicando estilos

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -8,13 +8,13 @@ function Main() {
                 <S.Input type="text" placeholder="Digite seu CPF" />
                 <S.Input type="email" placeholder="Informe seu email" />
                 <label>Selecione seu Estado</label>
-                <select>
+                <S.Select>
                     <option>SP</option>
                     <option>RN</option>
                     <option>RJ</option>
                     <option>BA</option>
                     <option>CE</option>
-                </select>
+                </S.Select>
             </S.Section>
         </S.Form>
     )
--- src/Components/Main/mainStyle.jsx
+++ src/Components/Main/mainStyle.jsx
@@ -13,5 +13,18 @@ export const Section = styled.div`
 `
 
 export const Input = styled.input`
+width: 20vw;
+height: 5vh;
+border-radius: 1rem;
+border: solid #6b0808cc 1px;
+color: #3c0404;
+text-align: center;
+`
 
+export const Select = styled.select`
+width: 20vw;
+height: 5vh;
+border-radius: 1rem;
+border: solid #6b0808cc 1px;
+color: #3c0404;
 `
