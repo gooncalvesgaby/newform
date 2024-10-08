2845488dc794b030cc8006ec89437c8c5e702ebc
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-10-08 15:50:40 -0300

    estilos e inclusão de inputs

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -4,17 +4,39 @@ function Main() {
     return(
         <S.Form>
             <S.Section>
-                <S.Input type="text" placeholder="Digite seu nome" />
-                <S.Input type="text" placeholder="Digite seu CPF" />
-                <S.Input type="email" placeholder="Informe seu email" />
-                <label>Selecione seu Estado</label>
-                <S.Select>
-                    <option>SP</option>
-                    <option>RN</option>
-                    <option>RJ</option>
-                    <option>BA</option>
-                    <option>CE</option>
-                </S.Select>
+                <div>
+                    <S.Input type="text" placeholder="Digite seu nome" />
+                    <S.Input type="text" placeholder="Digite seu CPF" />
+                    <S.Input type="email" placeholder="Informe seu email" />
+                </div>
+                <div>
+                    <S.Label>
+                        Selecione seu Estado
+                    </S.Label>
+                        <S.Select>
+                            <option>SP</option>
+                            <option>RN</option>
+                            <option>RJ</option>
+                            <option>BA</option>
+                            <option>CE</option>
+                        </S.Select>
+                    <S.Input type="password" placeholder="Definir Senha" />
+                    <S.Input type="password" placeholder="Confirmar Senha" />
+                </div>
+                <div>
+                    <S.Label>
+                        Informe seu Sexo
+                    </S.Label>
+                        <S.Select>
+                            <option>Feminino</option>
+                            <option>Masculino</option>
+                        </S.Select>
+                    <S.Input type="password" placeholder="Definir Senha" />
+                    <S.Input type="password" placeholder="Confirmar Senha" />
+                </div>
+                <S.Container>
+                    <S.Button>ENVIAR</S.Button>
+                </S.Container>
             </S.Section>
         </S.Form>
     )
--- src/Components/Main/mainStyle.jsx
+++ src/Components/Main/mainStyle.jsx
@@ -9,7 +9,17 @@ justify-content: center;
 `
 
 export const Section = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+gap: 3rem;
+`
 
+export const Label = styled.label`
+display: block;
+width: 15vw;
+text-align: center;
+font-size: 15px;
 `
 
 export const Input = styled.input`
@@ -19,6 +29,7 @@ border-radius: 1rem;
 border: solid #6b0808cc 1px;
 color: #3c0404;
 text-align: center;
+margin: 1rem;
 `
 
 export const Select = styled.select`
@@ -27,4 +38,18 @@ height: 5vh;
 border-radius: 1rem;
 border: solid #6b0808cc 1px;
 color: #3c0404;
+margin: 1rem;
+`
+
+export const Button = styled.button`
+width: 15vw;
+height: 5vh;
+border: solid 1px black;
+border-radius: 1rem;
+background: #ecbcbc;
+`
+
+export const Container = styled.div`
+display: flex;
+justify-content: center;
 `
